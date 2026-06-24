795ad5571eb3a9a83f7ae1bdd1281b08bae3b37f
Andrew Serbin <serbinadrian@outlook.com>
2026-06-24 11:48:54 +0300

    [TEMP] Added structure for signer implementation

--- scenarios/src/domains/Signer/index.ts
+++ scenarios/src/domains/Signer/index.ts
@@ -1,11 +1,42 @@
-export default class Signer {
-    private constructor() {}
+export enum SignerType {
+    HD = "HierarchicalDeterministic",
+    PK = "PrivateKey",
+}
 
-    public static createPrivateKeySigner(options: any): Signer {
+export type PrivateKeySignerCreateOptions = {
+    type: SignerType.PK;
+};
+
+export type HierarchicalDeterministicCreateOptions = {
+    type: SignerType.HD;
+};
+
+export default class Signer {
+    private readonly type: SignerType;
+    readonly secretMaterial: Uint8Array;
+
+    private constructor() {
+        this.type = SignerType.PK;
+        this.secretMaterial = new Uint8Array();
+    }
+
+    public static createPrivateKeySigner(
+        options: PrivateKeySignerCreateOptions,
+    ): Signer {
         return new Signer();
     }
-    
-    public static createHierarchicalDeterministicSigner(options: any): Signer {
+
+    public static createHierarchicalDeterministicSigner(
+        options: HierarchicalDeterministicCreateOptions,
+    ): Signer {
         return new Signer();
+    }
+
+    public async signMessage(
+        message: any,
+        passwordProviderInterface: any,
+        metadata: any,
+    ): Promise<any> {
+        // should return signed message and signature in a single object
     }
 }
